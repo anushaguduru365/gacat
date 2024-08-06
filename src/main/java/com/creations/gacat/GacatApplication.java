cf794ca99e19d0cf74ab1cc66fb3caa3c151e9fd
anushaguduru365 <anushaguduru365@gmail.com>
2024-08-06 22:16:36 +0530

    gacat application......

---
+++ src/main/java/com/creations/gacat/GacatApplication.java
@@ -0,0 +1,13 @@
+package com.creations.gacat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GacatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creations/gacat/GacatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creations.gacat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GacatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
