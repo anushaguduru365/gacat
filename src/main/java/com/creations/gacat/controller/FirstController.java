10e4e7185ead5b37c5f4f836c315e15a74d6d18f
anushaguduru365 <anushaguduru365@gmail.com>
2024-08-07 18:17:09 +0530

    getmapped changes..

--- src/main/java/com/creations/gacat/controller/FirstController.java
+++ src/main/java/com/creations/gacat/controller/FirstController.java
@@ -1,8 +1,25 @@
 package com.creations.gacat.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
+//need to write get method
+    @GetMapping("aa")
+    public String catRemo(){
+        //System.out.println("I got the catRemo .....");
+        return "I got the catRemo....yahooo..";
+    }
+
+//need to write delete method
+    @DeleteMapping("anu")
+    public String DogRemo(){
+        return "we can delete the mapping .....";
+    }
+//need to write post method
+    //@PostMapping
+//need to write put method
+   // @PutMapping
+
 
 }
