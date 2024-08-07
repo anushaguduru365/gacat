306639c66e49c398aa7d6cdcaea7ab330a915f86
anushaguduru365 <anushaguduru365@gmail.com>
2024-08-07 19:15:04 +0530

    all mapping changes..

--- src/main/java/com/creations/gacat/controller/FirstController.java
+++ src/main/java/com/creations/gacat/controller/FirstController.java
@@ -5,19 +5,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 //need to write get method
-    @GetMapping("aa")
+    @GetMapping("/aa")
     public String catRemo(){
         //System.out.println("I got the catRemo .....");
         return "I got the catRemo....yahooo..";
     }
 
 //need to write delete method
-    @DeleteMapping("anu")
+    @DeleteMapping("/ab")
     public String DogRemo(){
         return "we can delete the mapping .....";
     }
 //need to write post method
-    //@PostMapping
+    @PostMapping("/remo")
+  public String PostRemo(){
+    return "we can post the mapping .....";
+}
 //need to write put method
    // @PutMapping
 
